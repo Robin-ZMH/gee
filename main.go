371c27ad68d7730b2bc0e50a57b70a52286448fd
Robin <robin@example.com>
2022-12-29 22:17:55 +0800

    dev day1

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+// $ curl http://localhost:9999/
+// URL.Path = "/"
+// $ curl http://localhost:9999/hello
+// Header["Accept"] = ["*/*"]
+// Header["User-Agent"] = ["curl/7.54.0"]
+
+import (
+	"gee"
+	"log"
+)
+
+func main() {
+	engine := gee.New()
+	log.Fatal(engine.Run(":9000"))
+}
+
